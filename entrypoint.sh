b4723278e1eac9d9bdad7e244f7d078e960bf6c0
Rustem Suniev <rustem.suniev@6point6.co.uk>
2016-09-07 17:20:00 +0100

    Added echo to print return code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 while true; do
-  curl -sL -w "%{http_code}" -o /dev/null "$URL_TO_CURL"
+  HTTP_CODE=$(curl -sL -w "%{http_code}" -o /dev/null "$URL_TO_CURL")
+  echo "Returned code: $HTTP_CODE"
   sleep 0.1
 done;
