928e26210a8a369eb1dd8273e1a4c8a4501a67c0
Rustem Suniev <rustem.suniev@6point6.co.uk>
2016-09-08 18:23:14 +0100

    Added output to syslog

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+host=$(hostname)
+
 while true; do
   HTTP_CODE=$(curl -sL -w "%{http_code}" -o /dev/null "$URL_TO_CURL")
   echo "Returned code: $HTTP_CODE"
+  logger -t "$host" $HTTP_CODE
   sleep 0.1
 done;
