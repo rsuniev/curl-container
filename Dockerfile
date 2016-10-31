12c3c4ac56c43d6473d056d3669d5d4627aeb671
Rustem Suniev <rustem.suniev@6point6.co.uk>
2016-10-31 13:16:33 +0000

    Added secret retrieval for rancher secrets bridge

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:trusty
 ENV NO_PROXY .svc.cluster.local,.service.consul
 ARG URL_TO_CURL
-#RUN apk add --update curl && rm -rf /var/cache/apk/*
-RUN apt-get update && apt-get install -y curl && apt-get clean && rm -rf /var/lib/apt/lists
+ARG VAULT_SERVER_URL
+RUN apt-get update && apt-get install -y curl jq && apt-get clean && rm -rf /var/lib/apt/lists
 COPY entrypoint.sh /
 CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+SERVER_URL='http://rancher-metadata'
+echo "Checking network is up on: $SERVER_URL"
+while true;
+  do
+    sleep 1
+    echo "Running curl"
+    HTTP_CODE=$(curl -m 1 -o /dev/null -s -w %{http_code} $SERVER_URL)
+    echo "http code: ${HTTP_CODE}"
+    if [ ${HTTP_CODE} -eq 200 ]; then
+      echo "Connectivity test has been successfull"
+      break
+    fi
+done
+
+
+SECRETS_FILE="/tmp/secrets.txt"
+TRY=0
+until [ -e $SECRETS_FILE ] || [ $TRY -gt 10 ]; do
+	echo "Waiting for openam boostrap file at $BOOTSTRAP_FILE"
+	sleep 2
+	let "TRY++"
+done
+
+if [ $TRY -lt 10 ]; then
+	echo "Found secrets file"
+else
+	echo "Didn't find secrets file. Setup has failed"
+  exit 1
+fi
+
+source $SECRETS_FILE
+PERM_KEY=$(curl -s -H "X-Vault-Token: $TEMP_TOKEN" $CUBBY_PATH |  jq -r '.data.permKey')
+SECRET_VALUE=$(curl -H "X-Vault-Token: $PERM_KEY" $VAULT_SERVER_URL/v1/secret/security/default/testusername | jq -r '.data.value')
+$SECRET_VALUE >> /temp/curl/security/default/secret.txt
+echo "Secret retrieved and saved"
+
 host=$(hostname)
 
 while true; do
